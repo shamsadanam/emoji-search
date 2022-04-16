fb07e2e18f0b2073ce5b4240f1921fa12d8ee538
Shamsad Anam <shamsad.anam@gmail.com>
2022-04-17 02:03:17 +0600

    adding group sorting

--- client/src/components/EmojiList.js
+++ client/src/components/EmojiList.js
@@ -2,13 +2,41 @@ import React from "react";
 import Emoji from "./Emoji";
 
 const EmojiList = ({ emojiList }) => {
+  var unique = [];
+  var distinct = [];
+  for (let i = 0; i < emojiList.length; i++) {
+    if (!unique[emojiList[i].group]) {
+      distinct.push(emojiList[i].group);
+      unique[emojiList[i].group] = 1;
+    }
+  }
+
+  console.log(distinct);
+
   const renderEmoji = (list) => {
     return list.map(({ label, char, codes }) => {
       return <Emoji key={codes} label={label} char={char} />;
     });
   };
   return (
-    <div className="emoji-container">{emojiList && renderEmoji(emojiList)}</div>
+    <div className="emoji-container">
+      {/* <EmojiGroup /> */}
+      <div className="emoji-group-container">
+        <div className="emoji-group">
+          <Emoji char="😀" label="Sort By Smiles and Faces" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+          <Emoji char="😀" />
+        </div>
+      </div>
+      {emojiList && renderEmoji(emojiList)}
+    </div>
   );
 };
 
